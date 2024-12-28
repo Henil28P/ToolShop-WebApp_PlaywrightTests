cb442da0a826ba29e5b5c655c0b107a693db38a2
Henil <henilp467@gmail.com>
2024-12-28 14:31:50 +1100

    [tests/home.spec.ts]: add test to check the count of items displayed (total 9) using legacy locator and also non-locator assertion

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -10,4 +10,12 @@ test("Home page", async ({ page }) => {
 
     // Check the title of the page
     await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0") // tests the title present in the tab of the web
+
+    // Check the count of items displayed - Expect 9 items are displayed initially in the home page
+    // Consider: What kind of div each of the items are in or where are they in the DOM? How can we find them? - use the Developer tools hover functionality and hover over each element to inspect it
+    // Use the Legacy locator pattern (the . in .col-md-9 indicates the class name)
+    const productGrid = page.locator(".col-md-9"); // The const syntax will set productGrid as a variable which can never change
+    await expect(productGrid.getByRole("link")).toHaveCount(9); // Withint he product grid, go get all the links (<a> tags) and we expect that that should have a count of 9
+    // We can also use a non-locator assertion for the above
+    expect(await productGrid.getByRole("link").count()).toBe(9);
 });
