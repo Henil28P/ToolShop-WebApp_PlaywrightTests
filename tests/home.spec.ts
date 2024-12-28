86d98dbf420687d5ac51f106ac9ba7c825ccb2fd
Henil <henilp467@gmail.com>
2024-12-28 14:19:24 +1100

    [tests/home.spec.ts]: ensure the sign in link is present in the nav bar of the ToolShop web home page

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,4 +3,8 @@ import { test, expect } from "@playwright/test" // to interact with test and exp
 // Playwright test for title verification of home page
 test("Home page", async ({ page }) => {
     await page.goto("https://practicesoftwaretesting.com/"); // 'goto' command allows testers to go and browse the site
+
+    // Ensure the sign in link is present
+    // 1. Open up the site, right-click on "Sign In" link on top right in nav bar and click "Inspect" to open Developer tools and note down the data-test's value
+    await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
 });
