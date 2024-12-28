03fc4d30e2dea3f533d0a524e1baf37ba37b8787
Henil <henilp467@gmail.com>
2024-12-28 14:15:14 +1100

    [tests/home.spec.ts]: set up a Playwright test for home page of ToolShop and direct page to go to the site

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,1 +1,6 @@
 import { test, expect } from "@playwright/test" // to interact with test and expect in this ToolShop's home page test
+
+// Playwright test for title verification of home page
+test("Home page", async ({ page }) => {
+    await page.goto("https://practicesoftwaretesting.com/"); // 'goto' command allows testers to go and browse the site
+});
