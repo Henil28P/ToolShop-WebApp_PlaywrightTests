44971c4205bbf5c92b4de76fa48dbe52b0ce10df
Henil <henilp467@gmail.com>
2024-12-28 16:37:00 +1100

    [tests/auth.setup.ts]: creeate and save the authentication file 'customer01AuthFile' upon log in

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -25,4 +25,8 @@ setup("Create customer 01 auth", async ({ page, context }) => {
     // Confirm that the username is logged in successfully using assertion for username appearing in the site
     await expect(page.getByTestId("nav-menu")).toContainText("Jane Doe");
 
+    // 4. Save the authentication file
+    await context.storageState({ path: customer01AuthFile }); // when this runs, it'll create the folder .auth, then create the file customer01.json file and within this file is all the cookie info that that customer01 browser session has
+    // Make sure to include the .auth folder in the .gitignore file for it to be ignored as it will change everytime a new user is logged in to the ToolShop system
+    // and a new .json file will be created in the .auth folder
 });
