4e1cd52715b8d51a29be54b3754b14b63964dfcc
Henil <henilp467@gmail.com>
2024-12-28 14:59:25 +1100

    [tests/home.spec.ts]: refactor and structure the Home page test for ToolShop site using 'describe' block

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,27 +1,31 @@
 import { test, expect } from "@playwright/test" // to interact with test and expect in this ToolShop's home page test
 
 // Playwright test for title verification of home page
-test("Home page", async ({ page }) => {
-    await page.goto("https://practicesoftwaretesting.com/"); // 'goto' command allows testers to go and browse the site
+// Refactor and structure the test code using "describe" block used to group tests together
+// Before All, Before Each, After Each blocks - these are things that can be run within the describe block outside of a test context
+test.describe("Home page", () => {
+    test("Home page", async ({ page }) => {
+        await page.goto("https://practicesoftwaretesting.com/"); // 'goto' command allows testers to go and browse the site
 
-    // Ensure the sign in link is present
-    // 1. Open up the site, right-click on "Sign In" link on top right in nav bar and click "Inspect" to open Developer tools and note down the data-test's value
-    await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+        // Ensure the sign in link is present
+        // 1. Open up the site, right-click on "Sign In" link on top right in nav bar and click "Inspect" to open Developer tools and note down the data-test's value
+        await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
 
-    // Check the title of the page
-    await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0") // tests the title present in the tab of the web
+        // Check the title of the page
+        await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0") // tests the title present in the tab of the web
 
-    // Check the count of items displayed - Expect 9 items are displayed initially in the home page
-    // Consider: What kind of div each of the items are in or where are they in the DOM? How can we find them? - use the Developer tools hover functionality and hover over each element to inspect it
-    // Use the Legacy locator pattern (the . in .col-md-9 indicates the class name)
-    const productGrid = page.locator(".col-md-9"); // The const syntax will set productGrid as a variable which can never change
-    await expect(productGrid.getByRole("link")).toHaveCount(9); // Withint he product grid, go get all the links (<a> tags) and we expect that that should have a count of 9
-    // We can also use a non-locator assertion for the above
-    expect(await productGrid.getByRole("link").count()).toBe(9);
+        // Check the count of items displayed - Expect 9 items are displayed initially in the home page
+        // Consider: What kind of div each of the items are in or where are they in the DOM? How can we find them? - use the Developer tools hover functionality and hover over each element to inspect it
+        // Use the Legacy locator pattern (the . in .col-md-9 indicates the class name)
+        const productGrid = page.locator(".col-md-9"); // The const syntax will set productGrid as a variable which can never change
+        await expect(productGrid.getByRole("link")).toHaveCount(9); // Withint he product grid, go get all the links (<a> tags) and we expect that that should have a count of 9
+        // We can also use a non-locator assertion for the above
+        expect(await productGrid.getByRole("link").count()).toBe(9);
 
-    // Search for "Thor Hammer" (action) and check the result in the grid (assertion)
-    await page.getByTestId("search-query").fill("Thor Hammer"); // Fill the search field with "Thor Hammer"
-    await page.getByTestId("search-submit").click(); // Click the search button
-    await expect(productGrid.getByRole("link")).toHaveCount(1); // Ensures that only 1 product result is shown in the productGrid which is Thor Hammer
-    await expect(page.getByAltText("Thor Hammer")).toBeVisible(); // The AltText of the image can be our locator to test appearance of Thor Hammer tool picture in the product grid
+        // Search for "Thor Hammer" (action) and check the result in the grid (assertion)
+        await page.getByTestId("search-query").fill("Thor Hammer"); // Fill the search field with "Thor Hammer"
+        await page.getByTestId("search-submit").click(); // Click the search button
+        await expect(productGrid.getByRole("link")).toHaveCount(1); // Ensures that only 1 product result is shown in the productGrid which is Thor Hammer
+        await expect(page.getByAltText("Thor Hammer")).toBeVisible(); // The AltText of the image can be our locator to test appearance of Thor Hammer tool picture in the product grid
+    });
 });
