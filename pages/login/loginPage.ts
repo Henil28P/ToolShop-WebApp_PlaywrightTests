a27e788fadc03f6a0178e38a1919a29d4da982bd
Henil <henilp467@gmail.com>
2024-12-29 23:18:03 +1100

    [pages/login/loginPage.ts]: add page object for automating login functionality test

--- pages/login/loginPage.ts
+++ pages/login/loginPage.ts
@@ -1,1 +1,26 @@
 import { type Locator, type Page } from "@playwright/test";
+
+// Constructor with the locators
+export class LoginPage {
+    readonly page: Page;
+    readonly emailInput: Locator;
+    readonly passwordInput: Locator;
+    readonly loginButton: Locator;
+    constructor(page: Page) {
+        this.page = page;
+        this.emailInput = page.getByTestId("email");
+        this.passwordInput = page.getByTestId("password");
+        this.loginButton = page.getByTestId("login-submit");
+    }
+
+    async goto() {
+        await this.page.goto("https://practicesoftwaretesting.com" + "/auth/login");
+    }
+
+    async login(email: string, password: string) {
+        // input the email and password into their respective fields when 'login' function is called
+        await this.emailInput.fill(email);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click(); // click the login button
+    }
+}
