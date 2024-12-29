7bd898ba08f0c099e86f388ca726fa2241e1b38f
Henil <henilp467@gmail.com>
2024-12-29 20:21:59 +1100

    [tests/checkout.spec.ts]: navigate the page to ToolShop website in a 'beforeEach' block

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -3,4 +3,10 @@ import { test, expect } from "@playwright/test";
 test.describe("Checkout challenge", async () => {
     // Use the storageState from the .auth/customer01.json which is what gets generated in our auth.setup.ts file
     test.use({ storageState: "ToolShop-WebApp_PlaywrightTests/.auth/customer01.json" });
+
+
+    // Let the page to go to the ToolShop website
+    test.beforeEach(async ({ page }) => {
+        await page.goto("https://practicesoftwaretesting.com");
+    });
 });
