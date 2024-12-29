941180a4a916f4af4bd00e78d38790a5555543c3
Henil <henilp467@gmail.com>
2024-12-29 20:26:02 +1100

    [tests/checkout.spec.ts]: add test for automating checkout process and find and add Claw Hammer tool to cart and validate quantity of cart is 1 by assertion and click on cart on nav bar

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -9,4 +9,12 @@ test.describe("Checkout challenge", async () => {
     test.beforeEach(async ({ page }) => {
         await page.goto("https://practicesoftwaretesting.com");
     });
+
+    // Test for automating checkout process
+    test("Buy now pay later", async ({ page }) => {
+        await page.getByText("Claw Hammer with Shock Reduction Grip").click(); // Find the Claw Hammer
+        await page.getByTestId("add-to-cart").click(); // Add the Claw Hammer to cart
+        await expect(page.getByTestId("cart-quantity")).toHaveText("1"); // Make an assertion to validate the cart quantity is 1
+        await page.getByTestId("nav-cart").click(); // Click on the nav-cart (cart icon on nav bar to check the cart)
+    });
 });
