be6454e881ff3d2a4c349a9c1cea3b9bfb228a7d
Henil <henilp467@gmail.com>
2024-12-29 20:15:41 +1100

    [tests/checkout.spec.ts]: use the storageState from the .auth/customer01.json which is generated in the auth.setup.ts file and debug imports by adding a missing ';'

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -1,1 +1,6 @@
-import { test, expect } from "@playwright/test"
+import { test, expect } from "@playwright/test";
+
+test.describe("Checkout challenge", async () => {
+    // Use the storageState from the .auth/customer01.json which is what gets generated in our auth.setup.ts file
+    test.use({ storageState: "ToolShop-WebApp_PlaywrightTests/.auth/customer01.json" });
+});
